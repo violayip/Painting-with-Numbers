6de31765969a4606c099af1679889d37826a8de3
violayip <violayiptinwai@gmail.com>
2017-03-19 11:19:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+function setup() {
+  createCanvas (750,600);
+  
+}
+
+function draw() {
+  background (0);
+  stroke (255,255,255);
+  noFill ();
+  ellipse(125, 270, 80,80);
+  ellipse (110,290,10,50);
+  ellipse (125,290,10,50);
+  ellipse (140,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (100,300, 50,20);
+  stroke (255,255,255);
+  line (100,300,150,300);
+  line (100,310,150,310);  
+  line (100,320,150,320);
+  
+  stroke (244, 194, 66);
+  noFill ();
+  ellipse(225, 270, 80,80);
+  ellipse (210,290,10,50);
+  ellipse (225,290,10,50);
+  ellipse (240,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (200,300, 50,20);
+  stroke (244, 194, 66);
+  line (200,300,250,300);
+  line (200,310,250,310);  
+  line (200,320,250,320);
+  
+  stroke (107, 244, 66);
+  noFill ();
+  ellipse(325, 270, 80,80);
+  ellipse (310,290,10,50);
+  ellipse (325,290,10,50);
+  ellipse (340,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (300,300, 50,20);
+  stroke (107, 244, 66);
+  line (300,300,350,300);
+  line (300,310,350,310);  
+  line (300,320,350,320);
+  
+  stroke (170, 27, 252);
+  noFill ();
+  ellipse(425, 270, 80,80);
+  ellipse (410,290,10,50);
+  ellipse (425,290,10,50);
+  ellipse (440,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (400,300, 50,20);
+  stroke (170, 27, 252);
+  line (400,300,450,300);
+  line (400,310,450,310);  
+  line (400,320,450,320);
+  
+  stroke (214, 21, 149);
+  noFill ();
+  ellipse(525, 270, 80,80);
+  ellipse (510,290,10,50);
+  ellipse (525,290,10,50);
+  ellipse (540,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (500,300, 50,20);
+  stroke (214, 21, 149);
+  line (500,300,550,300);
+  line (500,310,550,310);  
+  line (500,320,550,320);
+  
+  stroke (76, 233, 247);
+  noFill ();
+  ellipse(625, 270, 80,80);
+  ellipse (610,290,10,50);
+  ellipse (625,290,10,50);
+  ellipse (640,290,10,50);
+  stroke (0)
+  fill (0);
+  rect (600,300, 50,20);
+  stroke (76, 233, 247);
+  line (600,300,650,300);
+  line (600,310,650,310);  
+  line (600,320,650,320);
+  
+  textSize(80);
+  textAlign(CENTER);
+  text("Good Ideas!", 375, 450);
+
+  
+}
